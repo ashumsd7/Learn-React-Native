af82142fabde5a1d9d0f32e4af757030177b8af6
aathym <aat8001@gmail.com>
2023-01-14 13:50:35 +0530

    add todo ui elements

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
 // import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={( styles.styleLine,styles.text)}>React on Native 💙</Text>
-      <Text style={styles.styleLine}>Style this line</Text>
-      <Button title="Register" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Type your goal"/>
+        <Button title="add goal"/>
+      </View>
+      <View>
+        <Text>Your goals list</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ff3",
-
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding:50
   },
   text: {
     fontSize: 40,
     fontWeight: "bold",
     borderColor: "green",
-    borderWidth:1,
-    padding:10
+    borderWidth: 1,
+    padding: 10,
   },
   styleLine: { margin: 16, borderWidth: 1, borderColor: "green", padding: 10 },
 });
