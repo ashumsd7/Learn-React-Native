8a43000d21580567c48aa01d216fe91cf7860d57
aathym <aat8001@gmail.com>
2023-01-14 12:46:58 +0530

    add styled

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>************* </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>React on Native 💙</Text>
+      <Text style={styles.styleLine}>Style this line</Text>
+      <Button title="Register" />
     </View>
   );
 }
@@ -13,9 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff3',
-    color:'#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff3",
+
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  styleLine: { margin: 16, borderWidth: 1, borderColor: "green", padding: 10 },
 });
